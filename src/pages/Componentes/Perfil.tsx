415189e6d2942617fffe1e58ce4c1cbe0c3ec248
Lverise <diego.pereira.200317@gmail.com>
2024-06-18 11:46:40 -0400

    Initial Commit
    Se agrega prop basico para mostrar lo que hacen

---
+++ src/pages/Componentes/Perfil.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+interface Props{
+    nombre:string
+    apellido:string
+    edad:number
+    //al agregar un ? el parametro no es obligatorio (no da error)
+    rol?:"estudiante"|"docente"
+}
+
+
+export const Perfil = (props:Props) => {
+  return (
+    <>
+        <h1>Perfil</h1>
+        <p>Nombre: {props.nombre}</p>
+        <p>Apellido: {props.apellido}</p>
+        <p>Edad: {props.edad}</p>
+        { props.edad > 18 && <p>Si es mayor de edad</p>}
+        { props.edad <= 18 && <p>No es mayor de edad</p>}
+        {   props.rol!=undefined &&
+            <p>Eres: {props.rol}</p> &&
+            props.rol=="docente" &&
+            <ul>
+                <li>Subir Notas</li>
+                <li>Reprobar Alumno</li>
+                <li>Aprobar Alumno</li>
+            </ul>
+                }
+    </>
+  )
+}
+export default Perfil
