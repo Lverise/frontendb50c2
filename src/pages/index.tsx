b61bc81ffae74d328f997dee18da8ebd7f26f315
Lverise <diego.pereira.200317@gmail.com>
2024-06-25 11:53:19 -0400

    se trabaja con bootstrap react
    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
+import Link from "next/link";
 import Perfil from "./Componentes/Perfil";
-
 export default function Home() {
   return (
     <>
+    <nav>
+      <Link href="/Pagina1">Pagina1</Link>
+      <Link href= "/Pagina2">Calculadora</Link>
+    </nav>
      <p>Hola</p>
      <p>Chao</p>
      <Perfil nombre="Pablo" apellido="Marmol" edad={30} rol="docente"/>
      <Perfil nombre="José" apellido="Pérez" edad={15}/>
+
     </>
   
   );
